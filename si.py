fa0e8e6c02d9b81b4a6f04e83f7446f0f0444f88
Nikhil Daphale <nikhildaphale9@gmail.com>
2021-08-26 16:31:32 +0530

    Add files via upload

---
+++ si.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+
+"""
+
+x=int(input("Ënter the amount: "))
+y=float(input("Enter the rate: "))
+z=int(input("Enter the number of years: "))
+
+a=int(x*y*z)//100
+
+print(a)
+
+
+    
